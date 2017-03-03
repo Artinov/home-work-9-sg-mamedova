1a517edd419ca26f81800e397bc050d9a4342000
sg-mamedova <sabina.mamedova90@gmail.com>
2017-03-03 21:48:01 +0200

    done home work whith help video

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,16 @@
 var ball = document.querySelector(".discoball");
+var ballColors = ["here", "goes", "our", "awesome", "disco", "ball"]
+var i = 0;
 
 function goDisco() {
 	//do your disco magic here !
+	democlass = (ballColors[i]);
+	i++
+	if (i>=5) {
+		i = 0;
+	}
+	
+	ball.setAttribute("class", "discoball" + " " + democlass);
 }
 
-setInterval(goDisco,300);
+setInterval(goDisco,200);
